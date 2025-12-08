7021217dd47b5123088485f5496b80f3ca0ab035
kitoutou999 <tomdavid09022003@gmail.com>
2025-12-08 17:14:36 +0100

    fusion ajout robot

---
+++ CC2/Robot.java
@@ -0,0 +1,44 @@
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
+
+public class Robot extends Agent {
+
+    protected HashMap competences = new HashMap<String, Double>();
+
+	// Put agent initializations here
+	protected void setup() {
+        // Print a welcome message
+		System.out.println("Robot " + this.getAID().getName() + " is ready.");
+		this.addBehaviour(new myBehaviour(this, 10000)); //1000 = 1 sec
+
+        for 
+
+
+	}
+
+
+	// Put agent clean-up operations here
+	protected void takeDown() {
+		// Printout a dismissal message
+		System.out.println("Agent " + this.getAID().getName() + "terminating.");
+
+	}
+	
+	private class myBehaviour extends TickerBehaviour {
+
+		public myBehaviour(Agent a, long period) {
+			super(a, period);
+		}
+
+		protected void onTick() {
+			System.out.println("TICK");
+		}
+
+	}
+
+}
+
+
