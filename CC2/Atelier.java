628d5b0c4dd162a6f37fbdc65eb661cf532c024f
pasquierT <titouan.pasquier@etu.unicaen.fr>
2025-12-11 15:07:32 +0100

    atelier mieux

--- CC2/Atelier.java
+++ CC2/Atelier.java
@@ -12,10 +12,11 @@ public class Atelier extends Agent {
 	
 	private Random random = new Random();
 	
-	private int lambda1 = 1000;
-	private int lambda2 = 5000;
-    private int nbCompetencesActives = 2;
-    private int nbCompetencesTotal = 5;
+	private int lambda1;
+	private int lambda2;
+	private int nbCompetencesMaxProduit;
+	private int nbCompetencesTotal;
+    
 	
 	private int idProduit = 1;
 	
@@ -30,31 +31,46 @@ public class Atelier extends Agent {
             
             this.lambda1 = Integer.parseInt(prop.getProperty("lambda1", "1000"));
             this.lambda2 = Integer.parseInt(prop.getProperty("lambda2", "5000"));
-            this.nbCompetencesActives = Integer.parseInt(prop.getProperty("nbCompetencesActives", "2"));
-            this.nbCompetencesTotal = Integer.parseInt(prop.getProperty("nbCompetencesTotal", "5"));
+            
+            this.nbCompetencesTotal = Integer.parseInt(prop.getProperty("nbCompetencesTotal", "10"));
+            this.nbCompetencesMaxProduit = Integer.parseInt(prop.getProperty("nbCompetencesMaxProduit", "3"));
             
             input.close();
-            System.out.println("Atelier config chargée: L1=" + lambda1 + " L2=" + lambda2 + " Act=" + nbCompetencesActives + " Tot=" + nbCompetencesTotal);
+            registerDF();
+            System.out.println("Atelier config chargée: L1=" + lambda1 + " L2=" + lambda2);
             
         } catch (IOException ex) {
             System.out.println("Atelier: config.properties non trouvé, utilisation des valeurs par défaut.");
         }
 
 		System.out.println("Atelier Créé");
-		long temp = getRandomTemps();
-		this.addBehaviour(new TickerBehaviour(this, temp) { 
+		long temps = getRandomTemps();
+		this.addBehaviour(new TickerBehaviour(this, temps) { 
 		
 			@Override
 			protected void onTick(){
 				
 				//Créer un nouveau Produit à envoyer
 				Produit newProduit = new Produit("P" + idProduit);
-				idProduit++;
+				int nbCompetencesProduit = random.nextInt(nbCompetencesMaxProduit) + 1;
+				System.out.println("nbCompetencesProduit " + nbCompetencesProduit);
 				
+				for(int i = 0;i < nbCompetencesProduit;i++){
+					int competance = random.nextInt(nbCompetencesTotal) + 1;
+					newProduit.ajoutCompetence(competance);
+				}
+				
+				idProduit++;
 				System.out.println("Atelier tick temps = " + getPeriod() + " ms viens de créer le produit " + newProduit.getId());
 				
-				long temp = getRandomTemps(); //Créer une nouvelle durée aléatoire
-				reset(temp); //maj le temps avec le nouveau temps
+				//Donne le produit créer à un agent aléatoirement choisi
+				
+				
+				
+				
+				
+				long temps = getRandomTemps(); //Créer une nouvelle durée aléatoire
+				reset(temps); //maj le temps avec le nouveau temps
 				
 				
 			}
@@ -69,8 +85,24 @@ public class Atelier extends Agent {
 
 	}
 	
+	//Donne un temps entre lambda2 et lambda1
 	private long getRandomTemps(){
 		return random.nextInt((lambda2 - lambda1) + 1) + lambda1;
 	}
+	
+	//S'enregistre dans les pages jaunes
+	private void registerDF() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("atelier");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+    }
 
 }
